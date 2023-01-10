4f9daf8840654d5ddaad69a7275fcfcfe1d4bb1d
Mayowa Ajiboye <mayaajiboye@gmail.com>
2023-01-09 23:17:42 -0500

    complete lab

---
+++ index.js
@@ -0,0 +1,11 @@
+function divide(parameter1, parameter2){
+  return parameter1 / parameter2;;
+}
+
+divide(2000,10);
+
+const square = (parameter3) => parameter3 * parameter3
+square(2);
+
+const add = (parameter1, parameter2) => parameter1 + parameter2
+add(2,2);
